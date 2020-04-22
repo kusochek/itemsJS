c556868375e95137782844a0c8adb22d18ace575
Air <air@instyes-MacBook-Air.local>
2020-04-22 13:10:51 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { items } from './data/items';
 import { createItemsList, searchElement } from './components/items-list-view'
 
 (async() => {
-    await createItemsList(items, searchElement);
+    await createItemsList(items);
     await searchElement();
 })();
