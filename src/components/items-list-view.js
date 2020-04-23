79807cc13c18fc821fa3bbdd67f41c7dafe10f31
Air <air@instyes-MacBook-Air.local>
2020-04-23 12:25:19 +0300

    edit

--- src/components/items-list-view.js
+++ src/components/items-list-view.js
@@ -23,24 +23,7 @@ export function createItemsList(items) {
     })
 }
 
-export function searchElement() {
-    document.querySelector('#search').oninput = function() {
-        let val = this.value.toLowerCase().trim();
-        const items = document.querySelectorAll('.d-flex', '.shadow');
-
-        if(val != '') {
-            items.forEach(item => {
-                const child = item.querySelector('.p-2', '.col-2');
-                if(child.innerText.toLowerCase().search(val) === -1) {
-                    item.classList.add('d-lg-none');
-                } else { 
-                    item.classList.remove('d-lg-none');
-                }
-            })
-        } else {
-            items.forEach(item => {
-                 item.classList.remove('d-lg-none');
-            })
-        }
-    }
+export function getItemsForSearch(searchElement) {
+    const items = document.querySelectorAll('.d-flex', '.shadow');
+    document.querySelector('#search').oninput = searchElement(items);
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import { items } from './data/items';
-import { createItemsList, searchElement } from './components/items-list-view'
+import { createItemsList, getItemsForSearch } from './components/items-list-view'
 
 (async() => {
     await createItemsList(items);
-    await searchElement();
+
+    function searchElement(items) {
+        let val = this.value.toLowerCase().trim();
+
+        if(val != '') {
+            items.forEach(item => {
+                const child = item.querySelector('.p-2', '.col-2');
+                if(child.innerText.toLowerCase().search(val) === -1) {
+                    item.classList.add('d-lg-none');
+                } else { 
+                    item.classList.remove('d-lg-none');
+                }
+            })
+        } else {
+            items.forEach(item => {
+                 item.classList.remove('d-lg-none');
+            })
+        }
+    };
+    
+    await getItemsForSearch(searchElement);
 })();
